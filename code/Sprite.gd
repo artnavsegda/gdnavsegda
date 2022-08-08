822d03ef3d555220715c91e355aa4b238f1246f1
Art Navsegda <artnavsegda@gmail.com>
2022-08-09 00:47:09 +0300

    Update Sprite.gd

--- code/Sprite.gd
+++ code/Sprite.gd
@@ -4,3 +4,14 @@ var angular_speed = PI
 
 func _init():
 	print("Hello, world!")
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
