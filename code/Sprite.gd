5dd9116e0594b4867bcb9e68845f0e035fab5b2c
Art Navsegda <artnavsegda@gmail.com>
2022-08-09 03:17:06 +0300

    wtf

--- code/Sprite.gd
+++ code/Sprite.gd
@@ -6,14 +6,9 @@ func _init():
 	print("Hello, world!")
 
 func _process(delta):
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-
-	rotation += angular_speed * direction * delta
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
+
+func _on_Button_pressed():
+	set_process(not is_processing())
